85d0dbc8f4389ef9092df16b2ee1ff27d9c0b5b0
JaredOrTr <trujillojaredalexander@gmail.com>
2022-11-17 21:50:18 -0600

    It was added the notification library also it was changed the color of the buttons

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const burgerMenu = document.getElementById('burger');
 const navItems = document.getElementById('nav-items');
+const buttons = document.querySelectorAll('button');
+
 
 burgerMenu.addEventListener('click', () => {
     navItems.classList.toggle('nav-active');
     //burgerMenu.classList.toggle('active');
 });
+
+buttons.forEach(button => button.addEventListener('click', () => {
+    Swal.fire({
+        title: 'Item added!!',
+        text: 'You added an item to the cart',
+        confirmButtonText: 'Close',
+        icon: 'success'
+    });
+}));
