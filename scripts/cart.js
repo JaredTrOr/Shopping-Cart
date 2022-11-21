48be323e2ff2fbf8d08d79a2fd167df3ac5c5ab3
JaredOrTr <trujillojaredalexander@gmail.com>
2022-11-20 22:01:16 -0600

    Implementation of a render function to render the elements of the cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,9 +1,78 @@
-function getCartInformation(){
-    const cartInfo = JSON.parse(localStorage.getItem('shopping-cart'));
-    const numberOfItems = localStorage.getItem('number-items');
-    document.getElementById('number-items').innerHTML = numberOfItems;
+const cartInfo = JSON.parse(localStorage.getItem('shopping-cart'));
+document.getElementById('number-items').innerHTML = cartInfo.length;
+renderItems();
 
+function renderItems(){
+    const shoppingCart = document.querySelector('.shopping-cart');
+
+    const shoppingHeader = `
+    <div class="shopping-header">
+        <div class="title">
+            <p>Cart items</p>
+            <hr>
+        </div>
+    </div>
+    `;
+
+    let shoppingBody = '';
+    cartInfo.forEach(item => {
+        shoppingBody = shoppingBody + 
+        `
+            <div class="shopping-body">
+                <div class="item-info">
+                    <div class="info">
+                        <div class="img">
+                            <img src="${item.image}" alt="">
+                        </div>
+                        
+                        <div>
+                            ${item.info}
+                        </div>
+                    </div>
+                    <div class="options">
+                        <div>
+                            <button class="remove option-buttons">
+                                Remove item
+                            </button>
+                        </div>
+                        <div class="buy-now">
+                            <button class="buy option-buttons">
+                                Buy now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item-amount flexing">
+                    <input type="number" value="1" min="0">
+                </div>
+
+                <div class="item-price flexing"> 
+                    ${item.price}
+                </div>
+            </div>
+        `;
+    });
+
+    const shopTotal = `
+    <div class="shop-total">
+        <div>
+            Total: <span id="total"></span>
+        </div>
+        <button id="buy-button" class="buy-total">Buy</button>
+    </div>
     
+    `
+
+    shoppingCart.innerHTML = shoppingHeader + shoppingBody + shopTotal;
+
+    //The appendChild only works with node elements and doesn´t work with strings
 }
 
-getCartInformation();
+function updateCartTotal(){
+
+}
+
+function numberOfEachItem(items){
+    
+}
